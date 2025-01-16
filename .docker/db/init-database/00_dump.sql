878e867d747ab1ce3b5af1e5fa62b4621b9f5d77
Hisham <hhuneidie@gmail.com>
2025-01-16 20:14:55 +0100

    fix(dump-sql): Dump had a bug

--- .docker/db/init-database/00_dump.sql
+++ .docker/db/init-database/00_dump.sql
@@ -14,6 +14,6 @@ CREATE TABLE IF NOT EXISTS User (
 
 -- Create tables
 
-INSERT INTO User(id, username, email, password) VALUES
+INSERT INTO User(id, username, email, password, status) VALUES
 ('df692f78-b179-4183-97e5-87f97024ffba', 'hisham.huneidie', 'hisham@example.com', 'hashed_password_123', 'ACTIVE'),
 ('a3c46212-bb70-43ac-982d-d6dbe447f648', 'faisal.ramones', 'faisal@example.com', 'hashed_password_456', 'PAUSED');
